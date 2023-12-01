a4e4a3f22641999aba3ab0e083a673b7809f514d
Ben Hoskings <bensebandraf@gmail.com>
2023-12-01 18:26:32 +0000

    simplify branch

--- web_application/ui.R
+++ web_application/ui.R
@@ -2,8 +2,14 @@ base::options(spinner.color = v_main_colour)
 base::options(spinner.type = 4)
 
 ui <- shiny::fluidPage(
-  waiter::useWaiter(),
+  title = "",
+  theme = shinythemes::shinytheme("flatly"),
 
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "app_style.css")
+  ),
+
+  waiter::useWaiter(),
   waiter::waiter_show_on_load(
     html = html_loading_screen,
     color = v_main_colour
@@ -13,207 +19,62 @@ ui <- shiny::fluidPage(
     text = "The app stopped working??"
   ),
 
-  # shinyBS:::shinyBSDep,
-  shinyWidgets::useSweetAlert(),
-  htmlwidgets::getDependency("sparkline"),
-  shinytoastr::useToastr(),
-  shinyFeedback::useShinyFeedback(),
-  shinyWidgets::setBackgroundColor("#F5F9FC"),
-  setShadow(id = "TabBox01"),
-
-  title = "",
-  theme = shinythemes::shinytheme("flatly"),
-
-
-  tags$style("
-    @import url(https://use.fontawesome.com/releases/v5.7.2/css/all.css);
-
-    table, th, td {
-      border-collapse: collapse;
-      border: 2px solid; text-align: center;
-    }
-
-    td, th, tr, tbody {
-        padding: .5em; border: 2px solid;
-    }
-
-    well {
-      border-width:10px;
-    }
-
-    .tab_title {
-      color: #3F3F3F;
-    }
-
-    .type_container {
-      grid-area: type;
-      color: #3F3F3F; font-size: 12px; padding: 10px;
-      display: flex; justify-content: center; align-items: center;}
-
-    .dist_container {
-      grid-area: dist;
-      color: #3F3F3F;
-      font-size: 12px; padding: 10px;
-      display: flex; justify-content: center; align-items: center;}
-
-    .time_container {
-      grid-area: time;
-      color: #3F3F3F;
-      font-size: 12px; padding: 10px;
-      display: flex; justify-content: center; align-items: center;}
-
-    .img_container {
-      grid-area: image;
-      font-size: 48px; padding: 10px;
-      display: flex; justify-content: center; align-items: center;}
-
-    .activity_container {
-      display: grid;
-      grid-template-areas:
-        'image image type'
-        'image image dist'
-        'image image time';
-    }
-
-    .profile_pic_container {
-      grid-area: profile_pic;
-    }
-
-    .activity_overview_container {
-      grid-area: activity_overview;
-    }
-
-    .profile_container {
-      display: grid;
-      grid-template-areas:
-        'profile_pic activity_overview activity_overview'
-        'profile_pic activity_overview activity_overview';
-    }
-
-    .circle {
-      grid-area: name_holder;
-      width: 100px;
-      height: 100px;
-      line-height: 100px;
-      border-radius: 50%;
-      font-size: 50px;
-      border-style: solid;
-      border-width: 2px;
-      color: #3F3F3F;
-      text-align: center;
-    }
-
-    .week_dist_container {
-      grid-area: week_dist;
-    }
-
-    .month_dist_container {
-      grid-area: month_dist;
-    }
-
-    .team_container {
-      display: grid;
-      column-gap:5px;
-      grid-template-areas:
-        'name_holder week_dist'
-        'name_holder month_dist';
-    }
-    "),
-
-  # Authentication
-
-  shinymanager::auth_ui(
-
-    id = "auth",
-    tags_top = tags$div(
-      tags$h4("Coventry Godiva Athelete Portal", style="align:center"),
-      tags$img(
-        src = "Logo.png",
-        width = 100
-      )
-    ),
-    tags_botton = tags$div(
-      tags$p(
-        "For any questions, please contact ",
-        tags$a(
-          href = "mailto:benjaminhoskings@gmail.com",
-          target = "_top", "administator"
-        )
-      )
-    ),
-    choose_language = F
-  ),
-
-  shiny::tagList(
-    shiny::fluidRow(
-      shiny::column(
-        width=6,
-        shiny::br(),
-        shiny::p(shiny::strong("Athelete Tracking", style="font-size:50px;")),
-        shiny::span("Roberts Endurance Performace Squad", style="font-size:25px; color:grey80")
-      ),
-
-      shiny::column(
-        width=2,
-        align = "right",
-        shiny::br(),
-      ),
-
-      shiny::column(
-        width = 2,
-        align = "left",
-        shiny::br()
-        # shiny::img(
-        #   src="data/Logo.png",
-        #   height = 60,
-        #   width = 160,
-        #   style = "padding-bottom:1px margin-bottom:1px"
-        # )
-      ),
-
-      shiny::column(
-        width = 2,
-        align = "middle",
-
-        shiny::fluidRow(
-          shiny::br(style = "line-height:50px"),
-          shiny::actionButton(
-            inputId = "ab_header_info",
-            label = "",
-            icon = shiny::icon("envelope")
-          ),
-          shiny::actionButton(
-            inputId = "ab_email",
-            label = "",
-            icon = shiny::icon("envelope")
-          ),
-          shiny::actionButton(
-            inputId = "ab_screenshot",
-            label = "",
-            icon = shiny::icon("camera")
-          ),
-          shiny::actionButton(
-            inputId = "ab_logout",
-            label = "",
-            icon = shiny::icon("sign-out-alt")
-          )
-        ),
-        shiny::fluidRow(
-          shiny::strong("Last Data Update: ", v_last_data_update)
-        )
-      )
-    ),
-    shiny::br()
-  ),
-
-
-
   shiny::fluidRow(
     shinydashboard::tabBox(
-      id = "TabBox01",
-      selected = NULL,
       title = "",
       width = 12,
+      id = "TabBox01",
+
+      shiny::tabPanel(
+        title = tags$div(class="tab_title", "Team Overview"),
+        shiny::wellPanel(
+          id = "WP_Team_Overview",
+          style = "background: #FFFFFF",
+
+          tags$div(class="data_container", "TEAM REPS"),
+          tags$br(style="height: 50px"),
+
+          ## Team Table ------
+          tags$div(class="data_container",
+            tags$table(class="team_table", {
+              rows <- base::ceiling(base::nrow(member_data) / 4)
+              purrr::map(c(1:rows), function(row_idx) {
+                tags$tr(class="team_row",
+                  purrr::map(1:4, function(col_idx) {
+                    if (((row_idx-1)*4 + col_idx) <= base::nrow((member_data))) {
+                      member_id <- member_data$ID[(row_idx-1)*4 + col_idx]
+                      tags$td(class="team_data",
+                        f_create_team_overview(
+                          member_data |>
+                            dplyr::filter(ID == member_id) |>
+                            dplyr::pull(First_Name),
+                          member_data |>
+                            dplyr::filter(ID == member_id) |>
+                            dplyr::pull(Last_Name),
+                          activities_detailed |>
+                            dplyr::filter(Athlete_ID == member_id) |>
+                            dplyr::filter(start_date > Sys.Date() - lubridate::days(6)) |>
+                            dplyr::filter(sport_type == "Run") |>
+                            dplyr::pull(distance) |>
+                            sum() / 1000,
+                          activities_detailed |>
+                            dplyr::filter(Athlete_ID == member_id) |>
+                            dplyr::filter(start_date > Sys.Date() - lubridate::days(30)) |>
+                            dplyr::filter(sport_type == "Run") |>
+                            dplyr::pull(distance) |>
+                            sum() / 1000
+                        )
+                      )
+                    }
+                  })
+                )
+              })}
+            )
+          )
+          # create a 4xY grid of athlete info
+        )
+      ),
+
 
       # Personal Tab -------
       shiny::tabPanel(
@@ -377,9 +238,6 @@ ui <- shiny::fluidPage(
             inputId = "ti_activity_overview",
             label = "How was your session?"
           ),
-
-          # 0-10 how was your session
-
           shiny::fluidRow(
             shiny::column(
               12,
@@ -397,3 +255,152 @@ ui <- shiny::fluidPage(
     )
   )
 )
+
+
+# shiny::appendTab(
+#   inputId = "TabBox01",
+#   shiny::tabPanel(
+#     title = tags$div(class="tab_title", "Admin Tab"),
+#     shiny::wellPanel(
+#       id = "WP_Admin",
+#       style = "background: white",
+#       shiny::navlistPanel(
+#         id = "NP_Admin",
+#         well = F,
+#         widths = c(2, 10),
+#
+#         shiny::tabPanel(
+#           title = "Session Input",
+#
+#           shiny::h2("Input session details here!"),
+#           shiny::fluidRow(
+#             shiny::column(
+#               width = 6,
+#               shiny::dateInput(
+#                 inputId = "di_activity_date",
+#                 label = "Date of Session",
+#                 value = Sys.Date(),
+#                 format = "D d MM"
+#               ),
+#               shiny::selectInput(
+#                 inputId = "si_session_type",
+#                 label = "Session Type",
+#                 choices = c("Reps"="R", "Pyramid"="P"),
+#                 selected = c("Reps"="R")
+#               ),
+#               shiny::numericInput(
+#                 inputId = "ni_rep_dist",
+#                 label = "Rep Distance",
+#                 value = 1000,
+#                 min=100
+#               ),
+#               shiny::fluidRow(
+#                 shiny::column(
+#                   width = 6,
+#                   shiny::numericInput(
+#                     inputId = "ni_set_count",
+#                     label = "Set Count",
+#                     value = 1,
+#                     min=1
+#                   )
+#                 ),
+#                 shiny::column(
+#                   width = 6,
+#                   shiny::numericInput(
+#                     inputId = "ni_set_rest",
+#                     label = "Set Rest (seconds)",
+#                     value = 0,
+#                     min=1
+#                   )
+#                 )
+#               ),
+#               shiny::fluidRow(
+#                 shiny::column(
+#                   width = 6,
+#                   shiny::numericInput(
+#                     inputId = "ni_rep_count",
+#                     label = "Rep Count",
+#                     value = 6,
+#                     min = 1
+#                   )
+#                 ),
+#                 shiny::column(
+#                   width = 6,
+#                   shiny::numericInput(
+#                     inputId = "ni_rep_rest",
+#                     label = "Rep Rest (seconds)",
+#                     value = 60,
+#                     min=1
+#                   )
+#                 )
+#               )
+#             ),
+#             shiny::column(
+#               width=6,
+#               checkboxGroupInput(
+#                 "checkGroup",
+#                 label = "Athlete Select",
+#                 choices = {
+#                   names(member_data$Username) <- member_data$ID
+#                 }
+#               )
+#             )
+#           ),
+#
+#           shiny::br(),
+#           shiny::h4("Select who attended!")
+#         ),
+#
+#         shiny::tabPanel(
+#           title = "Second Tab",
+#           icon = shiny::icon("database"),
+#
+#           shiny::h2("What data is used in this app?")
+#         )
+#       )
+#     )
+#   )
+# )
+#
+# ## Add button to set as default ----
+# output$session_buttons <- shiny::renderUI(
+#   shiny::fluidRow(
+#     shiny::column(
+#       width=6,
+#       shiny::actionButton(
+#         inputId = "ab_submit_session",
+#         label = "Submit Session"
+#       )
+#     ),
+#     shiny::column(
+#       width=6,
+#       shiny::actionButton(
+#         inputId = "ab_session_default",
+#         label = "Set As Default"
+#       )
+#     )
+#   )
+# )
+#
+# } else {
+#   output$session_buttons <- shiny::renderUI(
+#     shiny::fluidRow(
+#       shiny::column(
+#         width=12,
+#         shiny::actionButton(
+#           inputId = "ab_submit_session",
+#           label = "Submit Session"
+#         )
+#       )
+#     )
+#   )
+#
+#   updateSelectInput(
+#     session, "si_person_filter",
+#     label = "Select Athelete",
+#     choices = c(user_data$strava_id)
+#   )
+# }
+# }
+# }
+# )
